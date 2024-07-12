ddd96cbc1e820a614006bb1167d36872618a8ea3
Gabriel <glima5851@gmail.com>
2024-07-12 18:48:27 -0300

    update

---
+++ 02-tipos-e-variaveis/src/TiposVariaveis.java
@@ -0,0 +1,17 @@
+public class TiposVariaveis {
+    public static void main(String[] args){
+        byte idade = 123;
+		short ano = 2021;
+		int cep = 21070333;
+		long cpf = 98765432109L;
+		final float PI = 3.14F;
+		double salario = 1275.33;
+
+        System.out.println(idade);
+        System.out.println(ano);
+        System.out.println(cep);
+        System.out.println(cpf);
+        System.out.println(PI);
+        System.out.println(salario);
+    }
+}
