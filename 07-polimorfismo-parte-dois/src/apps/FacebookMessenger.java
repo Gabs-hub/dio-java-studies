ffc47053e1c844433ee9470282bfbca64c05be72
Gabriel <glima5851@gmail.com>
2024-07-13 23:46:51 -0300

    update

---
+++ 07-polimorfismo-parte-dois/src/apps/FacebookMessenger.java
@@ -0,0 +1,18 @@
+package apps;
+
+public class FacebookMessenger extends ServicoMensagemInstantanea {
+	public void enviarMensagem() {
+		validarConectadoInternet();
+		System.out.println("Enviando mensagem pelo Facebook Messenger");
+	}
+
+	public void receberMensagem() {
+		System.out.println("Recebendo mensagem pelo Facebook Messenger");
+	}
+
+	@Override
+	protected void salvarHistoricoMensagem() {
+		// TODO Auto-generated method stub
+
+	}
+}
---
+++ 07-polimorfismo-parte-dois/src/apps/Telegram.java
@@ -0,0 +1,18 @@
+package apps;
+
+public class Telegram extends ServicoMensagemInstantanea {
+	public void enviarMensagem() {
+		validarConectadoInternet();
+		System.out.println("Enviando mensagem pelo Telegram");
+	}
+
+	public void receberMensagem() {
+		System.out.println("Recebendo mensagem pelo Telegram");
+	}
+
+	@Override
+	protected void salvarHistoricoMensagem() {
+		// TODO Auto-generated method stub
+
+	}
+}
