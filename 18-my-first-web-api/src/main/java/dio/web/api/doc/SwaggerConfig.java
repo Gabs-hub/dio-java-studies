0df4aacbbc6ff36d3508a1bbd32398e9eb3517f9
Gabriel <glima5851@gmail.com>
2024-07-27 00:00:33 -0300

    update

---
+++ 18-my-first-web-api/src/main/java/dio/web/api/doc/SwaggerConfig.java
@@ -0,0 +1,29 @@
+package dio.web.api.doc;
+
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Title - Rest API")
+                        .description("API exemplo de uso de Springboot REST API")
+                        .version("1.0")
+                        .termsOfService("Termo de uso: Open Source")
+                        .license(new io.swagger.v3.oas.models.info.License()
+                                .name("Licença - Sua Empresa")
+                                .url("http://www.seusite.com.br"))
+                        .contact(new Contact()
+                                .name("Seu nome")
+                                .url("http://www.seusite.com.br")
+                                .email("voce@seusite.com.br"))
+                );
+    }
+}
