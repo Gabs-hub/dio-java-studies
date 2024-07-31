7260c49309c0cff1e75f8e2a3fe7390359185e25
Gabriel <glima5851@gmail.com>
2024-07-30 22:07:06 -0300

    update

---
+++ 21-desing-patters-java-puro/lab-padroes-projeto-java/src/one/digitalinovation/gof/SingletonLazyHolder.java
@@ -0,0 +1,17 @@
+package one.digitalinovation.gof;
+
+//Gostei desse ó
+public class SingletonLazyHolder {
+
+    private static class InstanceHolder{
+        private static SingletonLazyHolder instancia = new SingletonLazyHolder();
+    }
+
+    private SingletonLazyHolder() {
+        super();
+    }
+
+    public static SingletonLazyHolder getInstancia(){
+        return InstanceHolder.instancia;
+    }
+}
